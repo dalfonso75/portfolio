8b0ed88f6bf4defbcd9b565807b18960bfd27f5c
desarrolloInnteria <dalfonso75>
2023-04-07 16:46:53 -0500

    add Footer

---
+++ components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./Footer.module.css";
+const Footer = () => {
+  return (
+    <div className={style.c_footer}>
+      <p>© 2023 Enrique Alfonso . All right reserved.</p>
+      <a className={style.footer__email} href="">
+        ea888416@gmail.com
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
