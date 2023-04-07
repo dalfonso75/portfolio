5dfd9a5e4695870d967440bd75a87a231155dd2b
desarrolloInnteria <dalfonso75>
2023-04-07 16:47:55 -0500

    add context scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import AppLayout from "@/components/AppLayout";
+import { SmoothScrollProvider } from "@/context/SmoothScroll.context";
+import "@/styles/globals.css";
+import "locomotive-scroll/dist/locomotive-scroll.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SmoothScrollProvider
+      options={{
+        smooth: true,
+        table: {
+          smooth: true,
+        },
+        smartphone: {
+          smooth: true,
+        },
+      }}
+    >
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </SmoothScrollProvider>
+  );
 }
