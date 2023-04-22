b29679f6264b379f6228c392d2f6ecb7adedb111
desarrolloInnteria <dalfonso75>
2023-04-22 00:14:08 -0500

    minor fix

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -31,7 +31,7 @@ const About = () => {
           >
             Linkedin
           </a>
-          <a className={styles.social_media__link} href="/">
+          <a className={styles.social_media__link} href="https://github.com/dalfonso75">
             Mi Curricum Vitae
           </a>
         </div>
