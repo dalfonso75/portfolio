00e3ab5bf56011a51ea78b7146b3e1bab656d6d6
desarrolloInnteria <dalfonso75>
2023-04-25 22:22:29 -0500

    add menu with framer motion

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,15 +1,53 @@
-import React from "react";
-import Image from "next/image";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import styles from "./Header.module.css";
 
+const menuItems = [
+  "About",
+  "Experience",
+  "Work",
+  "Education",
+  "Skills",
+  "Contact",
+];
+
 const Header = () => {
+  const [selectedMenuItem, setSelectedMenuItem] = useState(null);
+
   return (
     <nav className={styles.c_header}>
-      <Image src="/next.svg" width={120} height={70} />
       <ul className={styles.header__links}>
-        <li className={styles.header__link}>Home</li>
-        <li className={styles.header__link}>Work</li>
-        <li className={styles.header__link}>Contact</li>
+        {menuItems.map((item) => (
+          <li
+            key={item}
+            className={styles.header__link}
+            onMouseEnter={() => setSelectedMenuItem(item)}
+          >
+            {item === selectedMenuItem && (
+              <motion.div
+                className={styles.header__underline}
+                layoutId="underline"
+                transition={{
+                  type: "spring",
+                  bounce: 0.1,
+                  duration: 0.5,
+                }}
+              />
+            )}
+            <motion.span
+              animate={{
+                color: item === selectedMenuItem ? "#fff" : "#8c8c8c",
+              }}
+              transition={{
+                duration: 0.35,
+                ease: [0.34, 0.31, 0.23, 0.96],
+                delay: 0.1,
+              }}
+            >
+              {item}
+            </motion.span>
+          </li>
+        ))}
       </ul>
     </nav>
   );
